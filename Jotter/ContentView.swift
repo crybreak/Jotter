464266e7a455cbf1c645a48130945a4bdb2cf588
Wilfried GOSSAN <gossanguy1@gmail.com>
2023-11-07 15:50:35 +0100

    Fetch data from Note

--- Jotter/ContentView.swift
+++ Jotter/ContentView.swift
@@ -11,68 +11,39 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<>
+    @FetchRequest(fetchRequest: Note.fetch(.all))
+    private var notes: FetchedResults<Note>
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(items) { item in
+                ForEach(notes) { note in
                     NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                        Text(" New Note at \(note.creationDate!)")
                     } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+                        Text(note.creationDate!, formatter: itemFormatter)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .onDelete(perform: deleteNotes)
             }
             .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
                 ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    Button(action: addNote) {
+                        Label("Add Note", systemImage: "plus")
                     }
                 }
             }
-            Text("Select an item")
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+   
+    
+    private func addNote() {
+        let _ = Note(title: "New Note", context: viewContext)
     }
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    private func deleteNotes(offsets: IndexSet) {
+        offsets.map { notes[$0] }.forEach(viewContext.delete)
     }
 }
 
--- Jotter/Model/Note+Helper.swift
+++ Jotter/Model/Note+Helper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import CoreData
+
+extension Note {
+    var title: String {
+        get {
+            self.title_ ?? ""
+        } set (newValue) {
+            self.title_ = newValue
+        }
+    }
+    
+    convenience init(title: String, context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.title = title
+    }
+    
+    public override func awakeFromInsert() {
+        self.creationDate = Date() + TimeInterval()
+    }
+    
+    static func fetch(_ predicate: NSPredicate ) -> NSFetchRequest<Note> {
+        let request = NSFetchRequest<Note>(entityName: "Note")
+        
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Note.creationDate, ascending: true)]
+        request.predicate = predicate
+        return request
+    }
+}
