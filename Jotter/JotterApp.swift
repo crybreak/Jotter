fed6ca02cd30900a855317c3cdc1f603c1555ed5
Wilfried GOSSAN <gossanguy1@gmail.com>
2023-11-07 16:11:43 +0100

    Save data when the app is background

--- Jotter/JotterApp.swift
+++ Jotter/JotterApp.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 @main
 struct JotterApp: App {
+    @Environment(\.scenePhase) var scenePhase
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }.onChange(of: scenePhase) {newScenePhase in
+            newScenePhase == .background ? persistenceController.save() : print("Screen Mode .active or .inactive")
+            
         }
     }
 }
