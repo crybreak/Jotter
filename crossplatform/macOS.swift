04f3551eacc521e38f0b4b6247cc101e1a560a94
Wilfried GOSSAN <gossanguy1@gmail.com>
2023-11-10 11:35:59 +0100

    Cross plATEFORM for macos

---
+++ crossplatform/macOS.swift
@@ -0,0 +1,19 @@
+//
+//  macOS.swift
+//  Jotter
+//
+//  Created by macbook on 09/11/2023.
+//
+
+import SwiftUI
+
+typealias UIImage = NSImage
+
+extension Image {
+    
+    init(uiImage: UIImage) {
+        self.init(nsImage: uiImage)
+    }
+}
+
+
